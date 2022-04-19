6c17218b86b305f851dc4410185edf8b03a3ebac
Mahsudbek1994 <mikki.12022021@gmail.com>
2022-04-19 13:48:37 +0500

    first commit

---
+++ internship_first_task/bin/internship_first_task.dart
@@ -0,0 +1,120 @@
+import 'dart:io';
+
+import 'dart:math';
+//1_misol
+
+void main() {
+  // int a, b, c;
+  // a = 5;
+  // b = 10;
+  // c = a+b;
+  // print(c);
+  //
+
+  // 2 _ misol
+  // print("Enter your number ..");
+  //
+  // int? a = int.parse(stdin.readLineSync()!);
+  //
+  // print("Enter your second number...");
+  //
+  // int? b = int.parse(stdin.readLineSync()!);
+  // if (a==b){
+  // print("A  is equal to b");}
+  // else if(a != b){
+  // print("a is not equal to b");}
+  // else if(a > b){
+  // print("a is greater than b");}
+  // else if (a<b){
+  // print("a is less than b");}
+  // else if(a>=b){
+  // print("a is greater than or equal to b");}
+  // else if(a <= b){
+  // print("a is less than or equal to b");}
+
+  //3_misol
+ //  int a = 3;
+ // int  b = 4;
+ //  int c = a+b;
+ //  print(c);
+ //
+
+  // 4_misol
+  //
+  // print("Qancha gilam olmoqchisiz:");
+  //  double a = double.parse(stdin.readLineSync()!);
+  //  int b = 2000;
+  //  double c = a * b;
+  //  print("Sizning gilamingiz narxi $c so'm");
+
+  // 6_misol
+ // print("Yilni kiriting:");
+ //  int  y = int.parse(stdin.readLineSync()!);
+ //   if (y < 1918);
+ //   if( y% 4==0){
+ //     print( "Dasturchilar kuni 12.09."+ y.toString());
+ //   }
+ //   else{
+ //       print(" Dasturchilar kuni 13.09." + y.toString());
+ //   }
+
+//19.04.2022
+
+
+//3_ misol
+// print("Enter your number :");
+// int n = int.parse(stdin.readLineSync()!);
+//   int reminder, sum =0, temp;
+//   temp = n;
+//   // a loop to reverse a number
+//   while(n>0)
+//   {
+//     reminder = n % 10;  //get remainder
+//     sum = (sum*10)+reminder;
+//     n = n~/10;
+//   }
+//
+//   if(sum == temp)
+//   {
+//     print('Its A Palindrome number');
+//   }else{
+//     print('Its A Not Palindrome number');
+
+//   }
+
+// }
+
+//4_misol
+
+// print("Odamlar soni kiriting...");
+//  int n = int.parse(stdin.readLineSync()!);
+//  print("Sotib olingan xurmo soni kiriting...");
+//  int k = int.parse(stdin.readLineSync()!);
+// if (k % n == 0){
+//  print("Yes");
+// } else {
+//  print("No");
+// }
+
+// 7_misol
+
+  }
+class Students{
+  String? name;
+  String? course;
+  int? age;
+  int? faculty;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
